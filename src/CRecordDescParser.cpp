dfbce57c2f1ffab15fa9b00e08983ade92f4cc1a
David P. Riedel <davidp.riedel@gmail.com>
2023-01-07 09:52:52 -0500

    development: start laying out class files

--- src/CRecordDescParser.cpp
+++ src/CRecordDescParser.cpp
@@ -15,16 +15,28 @@
 //
 // =====================================================================================
 
+    /* This file is part of ModernCRecord. */
+
+    /* ModernCRecord is free software: you can redistribute it and/or modify */
+    /* it under the terms of the GNU General Public License as published by */
+    /* the Free Software Foundation, either version 3 of the License, or */
+    /* (at your option) any later version. */
+
+    /* ModernCRecord is distributed in the hope that it will be useful, */
+    /* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+    /* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+    /* GNU General Public License for more details. */
+
+    /* You should have received a copy of the GNU General Public License */
+    /* along with Extractor_Markup.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include "CRecordDescParser.h"
 
+#include "CPP_Record_DescBaseVisitor.h"
 
-//--------------------------------------------------------------------------------------
-//       Class:  CRecordDescParser
-//      Method:  CRecordDescParser
-// Description:  constructor
-//--------------------------------------------------------------------------------------
-CRecordDescParser::CRecordDescParser ()
-
+std::optional<CRecord> CRecordDescParser::ParseRecordDescFile (const fs::path& record_desc_path)
 {
-}  // -----  end of method CRecordDescParser::CRecordDescParser  (constructor)  ----- 
+	return {};
+}		// -----  end of method CRecordDescParser::ParseRecordDescFile  ----- 
+
 
--- src/CRecordDescParser.h
+++ src/CRecordDescParser.h
@@ -16,36 +16,62 @@
 //
 // =====================================================================================
 
-#include "CPP_Record_DescBaseVisitor.h"
+    /* This file is part of ModernCRecord. */
+
+    /* ModernCRecord is free software: you can redistribute it and/or modify */
+    /* it under the terms of the GNU General Public License as published by */
+    /* the Free Software Foundation, either version 3 of the License, or */
+    /* (at your option) any later version. */
+
+    /* ModernCRecord is distributed in the hope that it will be useful, */
+    /* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+    /* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+    /* GNU General Public License for more details. */
+
+    /* You should have received a copy of the GNU General Public License */
+    /* along with Extractor_Markup.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef  _CRECORDDESCPARSER_INC_
+#define  _CRECORDDESCPARSER_INC_
+
+#include <filesystem>
+#include <optional>
+
+namespace fs = std::filesystem;
+
+#include "CRecord.h"
 
 
 // =====================================================================================
 //        Class:  CRecordDescParser
-//  Description: 
+//  Description:  Maybe just make this a standalone function -- too Java-like, this.  
 //
 // =====================================================================================
 class CRecordDescParser
 {
-	public:
-		// ====================  LIFECYCLE     ======================================= 
-		CRecordDescParser ();                             // constructor 
+public:
+	// ====================  LIFECYCLE     ======================================= 
+	CRecordDescParser () = default;                             // constructor 
 
-		// ====================  ACCESSORS     ======================================= 
+	// ====================  ACCESSORS     ======================================= 
 
-		// ====================  MUTATORS      ======================================= 
+	// ====================  MUTATORS      ======================================= 
 
-		// ====================  OPERATORS     ======================================= 
+	// ====================  OPERATORS     ======================================= 
 
-	protected:
-		// ====================  METHODS       ======================================= 
+    static std::optional<CRecord> ParseRecordDescFile(const fs::path& record_desc_path);
 
-		// ====================  DATA MEMBERS  ======================================= 
+protected:
+	// ====================  METHODS       ======================================= 
 
-	private:
-		// ====================  METHODS       ======================================= 
+	// ====================  DATA MEMBERS  ======================================= 
 
-		// ====================  DATA MEMBERS  ======================================= 
+private:
+	// ====================  METHODS       ======================================= 
+
+	// ====================  DATA MEMBERS  ======================================= 
 
 }; // -----  end of class CRecordDescParser  ----- 
 
+#endif   // ----- #ifndef _CRECORDDESCPARSER_INC_  ----- 
 
