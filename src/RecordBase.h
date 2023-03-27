1e6e44014f76bf6d690e6fdd0edb42e5eb110f51
David P. Riedel <davidp.riedel@gmail.com>
2023-03-27 15:16:56 -0400

    development: add utility functions
    - provide ability to access the underlying CField type.
    - provide ability convert a field's character data into a numeric type.

--- src/RecordBase.h
+++ src/RecordBase.h
@@ -18,6 +18,7 @@
 #ifndef  _BASERECORD_INC_
 #define  _BASERECORD_INC_
 
+#include <charconv>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -55,18 +56,50 @@ public:
 	const FieldList& GetFields() const { return fields_; }
 	size_t GetBufferLen() const { return buffer_size_; }
 
-    const FieldData& GetFieldData(std::string_view field_name) const
+    template<typename FLD_TYPE>
+    const FLD_TYPE& GetField(std::string_view field_name) const
     {
         auto pos = ranges::find_if(fields_, [field_name] (const auto& entry) { return entry.field_name_ == field_name; });
         if (pos != fields_.end())
         {
-            return *pos;
+            return std::get<FLD_TYPE>(pos->field_);
         }
         throw std::invalid_argument(fmt::format("Unknown field name: {}", field_name));
     }
-    const FieldData& GetFieldData(size_t which) const
+
+    template<typename FLD_TYPE>
+    const FLD_TYPE& GetField(size_t which) const
     {
-        return fields_.at(which);
+        return std::get<FLD_TYPE>(fields_.at(which).field_);
+    }
+
+    // convert the data to the desired numerical type -- INT or FLOAT most likely.
+    // this WILL THROW if the underlying data is not convertable to the specified
+    // numeric type
+
+    template<typename NBR_TYPE>
+    NBR_TYPE ConvertFieldToNumber(std::string_view field_name) const
+    {
+        const auto fld_data = (*this)[field_name];
+        NBR_TYPE base_fld_nbr;
+        auto [ptr, ec] = std::from_chars(fld_data.data(), fld_data.data() + fld_data.size(), base_fld_nbr);
+        if (ec != std::errc())
+        {
+            throw std::invalid_argument{fmt::format("Invalid 'array base field number': {}", fld_data)};
+        }
+        return base_fld_nbr;
+    }
+    template<typename NBR_TYPE>
+    NBR_TYPE ConvertFieldToNumber(size_t which) const
+    {
+        const auto fld_data = (*this)[which];
+        NBR_TYPE base_fld_nbr;
+        auto [ptr, ec] = std::from_chars(fld_data.data(), fld_data.data() + fld_data.size(), base_fld_nbr);
+        if (ec != std::errc())
+        {
+            throw std::invalid_argument{fmt::format("Invalid 'array base field number': {}", fld_data)};
+        }
+        return base_fld_nbr;
     }
 
 	// ====================  MUTATORS      ======================================= 
